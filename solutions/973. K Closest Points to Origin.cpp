79c42ff6e9ad3369170cd0bfe28e7afacc621fb0
ImUjjwalMittal <65807705+ImUjjwalMittal@users.noreply.github.com>
2021-05-12 13:20:06 +0530

    168 ms

---
+++ solutions/973. K Closest Points to Origin.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    int getDistance(int x , int y){
+        
+        return x * x + y * y ; 
+    }
+    
+    
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k){
+        
+        
+        priority_queue<pair<int , pair<int , int>>> pointsSortedByDistance  ; 
+        
+        int length = points.size();
+        
+      
+        
+        for(int i=0; i<length; i++){
+            
+            int distance = getDistance(points[i][0] , points[i][1]);
+            
+            int x = points[i][0];
+            int y = points[i][1];
+            
+            if(i<k){
+                 pointsSortedByDistance.push({distance , {x , y}});
+            }
+            else{
+                  pointsSortedByDistance.push({distance , {x , y}});
+                pointsSortedByDistance.pop();
+                
+​
+            }
+           
+        }
+        
+        int count = 0;
+        vector<vector<int>>ans;
